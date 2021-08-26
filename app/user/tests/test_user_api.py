47a7b6147af83145744fb423dedc7db5eaf638e5
Agnes Meri <agnes.meri@clicktravel.com>
2021-08-26 23:26:31 +0100

    correct typos and test setup

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -18,7 +18,7 @@ class PublicUserApiTests(TestCase):
     """Test the users API (public)"""
 
     def setUp(self):
-        self.client = APIClient
+        self.client = APIClient()
 
     def test_create_valid_user_success(self):
         """Test creating user with valid payload is successful"""
@@ -39,6 +39,7 @@ class PublicUserApiTests(TestCase):
         payload = {
             'email': 'someone@example.com',
             'password': 'testpass',
+            'name': 'Test',
             }
         create_user(**payload)
 
@@ -51,12 +52,13 @@ class PublicUserApiTests(TestCase):
         payload = {
             'email': 'someone@example.com',
             'password': 'pw',
+            'name': 'Test',
             }
 
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
-            email = payload['email']
+            email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
